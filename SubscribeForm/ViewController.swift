48a9a3c5b9972f20e95dd8837aec32820c189121
Yury Lebedev <quasaryy@gmail.comu>
2023-08-06 13:17:49 +0400

    The layout is ready

--- SubscribeForm/ViewController.swift
+++ SubscribeForm/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
